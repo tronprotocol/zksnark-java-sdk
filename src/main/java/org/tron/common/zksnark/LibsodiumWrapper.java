b7097a0b7a73a97bd8c650d92a540dc0b00b83c3
Matt <yrp1990@gmail.com>
2019-06-13 18:59:20 +0800

    load libzksnarkjni in libsodium

--- src/main/java/org/tron/common/zksnark/LibsodiumWrapper.java
+++ src/main/java/org/tron/common/zksnark/LibsodiumWrapper.java
@@ -1,7 +1,18 @@
 package org.tron.common.zksnark;
 
+import java.io.IOException;
+import org.tron.common.util.Utils;
+
 public class LibsodiumWrapper {
   private static final Libsodium INSTANCE = new Libsodium();
+
+  static {
+    try {
+      System.load(Utils.getLibraryByName("libzksnarkjni"));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   public static Libsodium getInstance() {
     return INSTANCE;
