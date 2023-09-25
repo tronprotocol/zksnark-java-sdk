f0aef8ec95550da9fcb3022e348b3c308a56f217
Alexander Filippov <aleksander.fill@gmail.com>
2023-09-25 16:16:09 +0300

    feat: update linux libs
    1. strip linux64/libzksnarkjni.so
    2. fix memcpy.c for aarch64 target
    3. update linux64/aarch64/libsnarkjni.so

--- cpp/src/memcpy.c
+++ cpp/src/memcpy.c
@@ -1,13 +1,17 @@
-#ifdef __linux__
-
 #include <string.h>
 
+#ifdef __linux__
+#if defined(__x86_64__) && defined(__GNU_LIBRARY__)
 void *__memcpy_old(void *, const void *, size_t);
 
 asm(".symver __memcpy_old, memcpy@GLIBC_2.2.5");
-void *__wrap_memcpy(void *dest, const void *src, size_t n)
-{
+void *__wrap_memcpy(void *dest, const void *src, size_t n) {
     return __memcpy_old(dest, src, n);
 }
-
+#else
+// https://chromium.googlesource.com/external/github.com/google/protobuf/+/HEAD/ruby/ext/google/protobuf_c/wrap_memcpy.c
+void *__wrap_memcpy(void *dest, const void *src, size_t n) {
+    return memmove(dest, src, n);
+}
+#endif
 #endif
